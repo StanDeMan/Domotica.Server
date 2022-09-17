1488fa0cb8a2880bf27ff67faa18ecc8e7140d78
StanDeMan <StanislausE@gmx.de>
2022-09-17 06:35:43 +0200

    Set to localhost and integrate TimeHub

--- Domotica.Server/Program.cs
+++ Domotica.Server/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSignalR();
 
 var app = builder.Build();
-app.MapHub<MainHub>("/current-time");
+app.MapHub<TimeHub>("/current-time");
 app.MapHub<Device>("/device");
 
 app.Run();
