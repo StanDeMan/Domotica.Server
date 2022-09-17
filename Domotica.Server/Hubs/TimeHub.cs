ad501c35ebced3c9392e5e198779c981b1e22067
StanDeMan <StanislausE@gmx.de>
2022-09-17 06:34:01 +0200

    Rename to TimeHub

---
+++ Domotica.Server/Hubs/TimeHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Runtime.CompilerServices;
+
+namespace Domotica.Server.Hubs
+{
+    public sealed class TimeHub : Hub
+    {
+        public async IAsyncEnumerable<DateTime> Streaming([EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            while (true)
+            {
+                yield return DateTime.Now;
+
+                try 
+                {
+                    await Task.Delay(1000, cancellationToken);
+                } 
+                catch (TaskCanceledException) 
+                {
+                    yield break;
+                }       
+            }
+        }
+    }
+}
