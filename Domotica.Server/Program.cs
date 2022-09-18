e5b10f6c2d365d79384a8d49e2939d1c04b458a4
StanDeMan <StanislausE@gmx.de>
2022-09-18 10:37:40 +0200

    Add cors

--- Domotica.Server/Program.cs
+++ Domotica.Server/Program.cs
@@ -3,8 +3,32 @@ using Domotica.Server.Hubs;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSignalR();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        corsPolicyBuilder =>
+        {
+            corsPolicyBuilder.WithOrigins("https://localhost:7077")
+                .AllowAnyHeader()
+                .WithMethods("GET", "POST")
+                .AllowCredentials();
+        });
+});
+
 var app = builder.Build();
-app.MapHub<TimeHub>("/current-time");
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseAuthorization();
+
+// UseCors must be called before MapHub.
+app.UseCors();
+
+app.MapHub<Time>("/current-time");
 app.MapHub<Device>("/device");
 
 app.Run();
